cf1257ae48d9972daf079ab1b6381337bdfe25fa
FrancescoDB <fdb4@njit.edu>
2023-09-12 15:55:25 -0400

    added script

--- healthcheckscript.py
+++ healthcheckscript.py
@@ -1,27 +1,19 @@
 
-from flask import Flask
-from healthcheck import he
+from flask import Flask, render_template
+from healthcheck import HealthCheck
 
+from flask import Flask, render_template
 app = Flask(__name__)
 
-health = HealthCheck()
-envdump = EnvironmentDump()
+@app.route('/projectserver/frontendproj')
+def mainpg():
+  return render_template('frontendpage.html')
 
-# add your own check function to the healthcheck
-def redis_available():
-    client = _redis_client()
-    info = client.info()
-    return True, "redis ok"
+@app.route('/projectserver/frontendproj/route')
+def my_link():
 
-health.add_check(redis_available)
 
-# add your own data to the environment dump
-def application_data():
-    return {"maintainer": "Luis Fernando Gomes",
-            "git_repo": "https://github.com/ateliedocodigo/py-healthcheck"}
+  return 'Click.'
 
-envdump.add_section("application", application_data)
-
-# Add a flask route to expose information
-app.add_url_rule("/healthcheck", "healthcheck", view_func=lambda: health.run())
-app.add_url_rule("/environment", "environment", view_func=lambda: envdump.run())
+if __name__ == '__main__':
+  app.run(debug=True)
